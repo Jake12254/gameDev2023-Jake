5ea29a3f102197ca2f372776cb9a9941e7125f7a
Jake12254 <123996060+Jake12254@users.noreply.github.com>
2023-05-11 13:00:48 -0700

    Update game.js

--- public/game.js
+++ public/game.js
@@ -41,8 +41,8 @@ function update() {
     // Calculate new x and y positions for the enemy
     var angle = Math.random() * Math.PI * 2;
     var distance = Math.random() * 50;
-    var dx = Math.cos(angle) * distance;
-    var dy = Math.sin(angle) * distance;
+    var dx = Math.cos(angle) * distance * 0.1;
+    var dy = Math.sin(angle) * distance; * 0.1;
     enemy.x += dx;
     enemy.y += dy;
 
