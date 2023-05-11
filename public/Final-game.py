185c6ee450f79ef5e120d67a3b9799a2ca7d48af
Jake12254 <123996060+Jake12254@users.noreply.github.com>
2023-05-11 12:41:48 -0700

    Update Final-game.py

--- public/Final-game.py
+++ public/Final-game.py
@@ -1,4 +1,13 @@
+import pygame
 from enemy import Enemy
+
+# Initialize Pygame
+pygame.init()
+
+# Set up the screen
+screen_width, screen_height = 800, 600
+win = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Enemy Path Example")
 
 # Create an instance of Enemy
 enemy = Enemy(100, 100, 50, 50, 5)
@@ -20,3 +29,5 @@ while running:
     # Update the display
     pygame.display.update()
 
+# Quit Pygame
+pygame.quit()
