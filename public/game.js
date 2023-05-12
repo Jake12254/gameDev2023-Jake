0e42c7090cdc47e94f038d7a74b3fe870a2205b9
Jake12254 <123996060+Jake12254@users.noreply.github.com>
2023-05-12 11:22:29 -0700

    Update game.js

--- public/game.js
+++ public/game.js
@@ -13,19 +13,61 @@ document.addEventListener("keyup", function (event) {
   keys[event.key] = false;
 });
 
-// Update the player position based on key states
-function updatePlayer() {
+// Set up the canvas and game loop
+var canvas = document.getElementById("gameCanvas");
+var ctx = canvas.getContext("2d");
+
+// Define the player object
+var player = {
+  x: 960,
+  y: 540,
+  width: 15,
+  height: 15,
+  speed: 5
+};
+
+// Define game objects
+var bullets = [];
+var enemies = [];
+
+// Spawn new enemies
+function spawnEnemy() {
+  var newEnemy = {
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    width: 15,
+    height: 15,
+    speed: 2
+  };
+  enemies.push(newEnemy);
+}
+
+// Spawn new bullets
+function spawnBullet() {
+  var newBullet = {
+    x: player.x + player.width / 2,
+    y: player.y,
+    width: 5,
+    height: 10,
+    speed: 10
+  };
+  bullets.push(newBullet);
+}
+
+// Update the game state
+function update() {
+  // Update the player position based on key states
   if (keys["w"]) {
-    player.y -= 5;
+    player.y -= player.speed;
   }
   if (keys["a"]) {
-    player.x -= 5;
+    player.x -= player.speed;
   }
   if (keys["s"]) {
-    player.y += 5;
+    player.y += player.speed;
   }
   if (keys["d"]) {
-    player.x += 5;
+    player.x += player.speed;
   }
 
   // Make sure the player stays within the canvas bounds
@@ -39,30 +81,64 @@ function updatePlayer() {
   } else if (player.y + player.height > canvas.height) {
     player.y = canvas.height - player.height;
   }
+
+  // Move game objects
+  bullets.forEach(function (bullet) {
+    bullet.y -= bullet.speed;
+  });
+
+  enemies.forEach(function (enemy) {
+    if (enemy.x < canvas.width / 2) {
+      enemy.x -= enemy.speed;
+    } else {
+      enemy.x += enemy.speed;
+    }
+  });
+
+  // Remove off-screen objects
+  bullets = bullets.filter(function (bullet) {
+    return bullet.y > 0;
+  });
+
+  enemies = enemies.filter(function (enemy) {
+    return enemy.x > -enemy.width && enemy.x < canvas.width + enemy.width;
+  });
+
+  // Check for collisions
+  // ...
+
+  // Spawn new enemies
+  if (Math.random() < 0.05) {
+    spawnEnemy();
+  }
+
+  // Spawn new bullets
+  if (keys[" "]) {
+    spawnBullet();
+  }
 }
 
-// Define game objects
+// Draw the game objects
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+ctx.fillStyle = "blue";
+ctx.fillRect(player.x, player.y, player.width, player.height);
 
-var bullets = [];
+for (let i = 0; i < bullets.length; i++) {
+  const bullet = bullets[i];
+  ctx.fillStyle = "red";
+  ctx.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
+}
 
-
-
-// Set up the canvas and game loop
-var canvas = document.getElementById("gameCanvas");
-var ctx = canvas.getContext("2d");
-
-// Define the player object
-var player = {
-  x: 960,
-  y: 540,
-  width: 15,
-  height: 15,
-  speed: 0.8
-};
-
-  // Update the game state
-  function update() {
-     updatePlayer();
+for (let i = 0; i < enemies.length; i++) {
+  const enemy = enemies[i];
+  ctx.beginPath();
+  ctx.arc(enemy.x, enemy.y, 10, 0, 2 * Math.PI);
+  ctx.fillStyle = "green";
+  ctx.fill();
+}
+// Update the game state
+function update() {
+  updatePlayer();
 
   // Check for collisions
   // ...
@@ -77,24 +153,21 @@ var player = {
   // ...
 
   // Move game objects
-    update();
   enemies.forEach(function (enemy) {
-  // Update the x-coordinate based on the enemy's position relative to the canvas
-  if (enemy.x < canvas.width / 2) {
-    enemy.x -= enemy.speed;
-  } else {
-    enemy.x += enemy.speed;
-  }
+    // Update the x-coordinate based on the enemy's position relative to the canvas
+    if (enemy.x < canvas.width / 2) {
+      enemy.x -= enemy.speed;
+    } else {
+      enemy.x += enemy.speed;
+    }
 
-  ctx.beginPath();
-  ctx.arc(enemy.x, enemy.y, 10, 0, 2 * Math.PI);
-  ctx.fillStyle = "red";
-  ctx.fill();
-});
+    ctx.beginPath();
+    ctx.arc(enemy.x, enemy.y, 10, 0, 2 * Math.PI);
+    ctx.fillStyle = "red";
+    ctx.fill();
+  });
 
   // Draw the game objects
-   
-    update();
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "blue";
   ctx.fillRect(player.x, player.y, player.width, player.height);
@@ -108,11 +181,6 @@ var player = {
     ctx.fillStyle = "green";
     ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
   });
-    
-    var enemies = [
-  { x: 100, y: canvas.height / 2, width: 15, height: 15, speed: 0.1 },
-  { x: canvas.width - 100, y: canvas.height / 2, width: 15, height: 15, speed: 0.1 },
-];
 }
 
 function loop() {
